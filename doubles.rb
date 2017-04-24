2729619c8ebb12668e0f726fc4abe909e3087dc8
quantumduck <quantum.duck@hotmail.com>
2017-04-24 18:24:58 -0400

    doubles

---
+++ doubles.rb
@@ -0,0 +1,21 @@
+class Die
+  attr_reader :sides, :showing
+
+  def initialize(sides = 6)
+    @sides = sides
+    roll
+  end
+
+  def roll
+    @showing = rand(@sides) + 1
+  end
+end
+
+d1 = Die.new
+d2 = Die.new
+
+puts "You rolled #{d1.roll} and #{d2.roll}."
+if d1.showing == d2.showing
+  puts "Doubles!"
+end
+puts "Your total is #{d1.showing + d2.showing}."
