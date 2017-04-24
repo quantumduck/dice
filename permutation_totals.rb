5f4f86d77de51cc2e256cd1c51e023516b436e0c
quantumduck <quantum.duck@hotmail.com>
2017-04-24 18:55:40 -0400

    totals

---
+++ permutation_totals.rb
@@ -0,0 +1,30 @@
+class Die
+  attr_reader :sides, :showing
+
+  def initialize(sides = 6)
+    @sides = sides
+    roll
+  end
+
+  def roll
+    @showing = rand(@sides) + 1
+  end
+
+  def permutations(n = 2)
+    (1..@sides**n).map do |permutation_number|
+      permutation = []
+      value = permutation_number - 1
+      m = n - 1
+      n.times do
+        permutation << value / @sides**(m) + 1
+        value %= @sides**(m)
+        m -= 1
+      end
+      permutation
+    end
+  end
+end
+
+d1 = Die.new
+
+d1.permutations.each { |p| puts "Dice Roll: #{p[0]}, #{p[1]}  Total: #{p[0]+p[1]}" }
